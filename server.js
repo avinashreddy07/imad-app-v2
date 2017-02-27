1db52f9ae836fc98e78ad62b1e0e9b19920c5401
avinashreddy07 <t.a.reddy7@gmail.com>
2017-02-27 23:13:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function (req, res) {
+    res.sendFile("Artical one requested and served");
+});
+
+app.get('/artical-two', function (req, res) {
+    res.sendFile("Artical 2 requested and served");
+});
+
+app.get('/artical-three', function (req, res) {
+    res.sendFile("Artical 3 requested and served");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
